023891eed87b8573172200078661f4999e0e57ac
Art <artiomtofancv@gmail.com>
2020-12-03 17:52:57 +0100

    fixed the main method

--- Dolphin.java
+++ Dolphin.java
@@ -2,9 +2,9 @@ import java.util.*; // for scanner
 import java.io.*; // for file 
 
 public class Dolphin {
-    static Menu Menu = new Menu();
-    static FileHandling FileHandling = new FileHandling();
-    public static ArrayList<Swimmer> mySwimmers = new ArrayList<>();;
+    private static Menu Menu = new Menu();
+    private static FileHandling FileHandling = new FileHandling();
+    private static ArrayList<Swimmer> mySwimmers = new ArrayList<>();;
 
     public static void main(String[] args) throws FileNotFoundException {
         FileHandling.readFromFile(mySwimmers);
